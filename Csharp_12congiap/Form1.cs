ca11a2fe2b39e48e315ac6f93a5116bcaac72f74
ADMIN <ADMIN@LAB1-MAY27>
2024-06-10 16:47:52 +0700

    Add project files.

---
+++ Csharp_12congiap/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_12congiap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string can = "";
+            switch (Convert.ToInt32(txt1.Text) % 10)
+            {
+                case 0:
+                    can = "Canh";
+                    break;
+                case 1:
+                    can = "Tân";
+                    break;
+                case 2:
+                    can = "Nhân";
+                    break;
+                case 3:
+                    can = "Quý";
+                    break;
+                case 4:
+                    can = "Giáp";
+                    break;
+                case 5:
+                    can = "Ất";
+                    break;
+                case 6:
+                    can = "Bính";
+                    break;
+                case 7:
+                    can = "Đinh";
+                    break;
+                case 8:
+                    can = "Mậu";
+                    break;
+                case 9:
+                    can = "Kỷ";
+                    break;
+            }
+            string chi = "";
+            switch(Convert.ToInt32(txt1.Text) % 12)
+            {
+                case 0:
+                    chi = "Thần";
+                    break;
+                case 1:
+                    chi = "Đậu";
+                    break;
+                case 2:
+                    chi = "Tuất";
+                    break;
+                case 3:
+                    chi = "Hợi";
+                    break;
+                case 4:
+                    chi = "Tý";
+                    break;
+                case 5:
+                    chi = "Sửu";
+                    break;
+                case 6:
+                    chi = "Dần";
+                    break;
+                case 7:
+                    chi = "Mão";
+                    break;
+                case 8:
+                    chi = "Thìn";
+                    break;
+                case 9:
+                    chi = "Tỵ";
+                    break;
+                case 10:
+                    chi = "Ngộ";
+                    break;
+                case 11:
+                    chi = "Mụi";
+                    break;
+            }
+            txtkq.Text = can + " " + chi;
+        }
+    }
+}
